d7d947ba982f58e95a73364d6303fce1c919d306
Ramenable <abelisable2@gmail.com>
2023-01-29 04:17:36 -0500

    Initial Commit

---
+++ Bard/BardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BardApp.swift
+//  Bard
+//
+//  Created by Abel Chen on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct BardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
