35f0cdb68fba8badcf9da384e8015651ece479aa
johntchen <jchen883@gatech.edu>
2023-01-30 23:16:08 -0500

    Implement login authentication

--- Bard/ContentView.swift
+++ Bard/ContentView.swift
@@ -8,32 +8,53 @@
 import SwiftUI
 
 let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+let storedUsername = "Bard"
+let storedPassword = "Chimes"
 
 struct ContentView: View {
     // Always read for changes
     @State var username: String = ""
     @State var password: String = ""
     
+    @State var authenticationDidFail: Bool = false
+    @State var authenticationDidSucceed: Bool = false
+    
     var body: some View {
-        // Vertical Stack
-        VStack {
-            WelcomeText()
-            ImageIcon()
-            TextField("Username", text: $username)
-                .padding()
-                .background(lightGreyColor)
-                .cornerRadius(5.0)
-                .padding(.bottom, 20)
-            SecureField("Password", text: $password)
-                .padding()
-                .background(lightGreyColor)
-                .cornerRadius(5.0)
-                .padding(.bottom, 20)
-            Button(action: {print("Login button tapped")}) {
-                LoginText()
+        ZStack {
+            // Vertical Stack
+            VStack {
+                WelcomeText()
+                ImageIcon()
+                UsernameTextField(username: $username)
+                PasswordSecureField(password: $password)
+                if authenticationDidFail {
+                    Text("Information not correct. Try again.")
+                        .offset(y: -10)
+                        .foregroundColor(.red)
+                }
+                Button(action: {
+                    print("Login button tapped")
+                    if self.username == storedUsername && self.password == storedPassword {
+                        self.authenticationDidSucceed = true
+                        self.authenticationDidFail = false
+                    } else {
+                        self.authenticationDidFail = true
+                    }
+                }) {
+                    LoginText()
+                    
+                }
+            }
+            .padding()
+            if authenticationDidSucceed {
+                Text("Login succeeded!")
+                    .font(.headline)
+                    .frame(width: 250, height: 80)
+                    .background(Color.green)
+                    .cornerRadius(20.0)
+                    .foregroundColor(.white)
             }
         }
-        .padding()
     }
 }
 
@@ -73,5 +94,27 @@ struct LoginText: View {
             .frame(width: 220, height: 60)
             .background(Color.green)
             .cornerRadius(15.0)
+    }
+}
+
+struct UsernameTextField: View {
+    @Binding var username: String
+    var body: some View {
+        TextField("Username", text: $username)
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
+    }
+}
+
+struct PasswordSecureField: View {
+    @Binding var password: String
+    var body: some View {
+        SecureField("Password", text: $password)
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
     }
 }
